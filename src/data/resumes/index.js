1a644deee3e2bb4b558683c517bed31be8b242bf
chadwithuhc <chadwithuhc@gmail.com>
2018-04-03 13:03:45 -0600

    Resume Updates

--- src/data/resumes/index.js
+++ src/data/resumes/index.js
@@ -1,9 +1,11 @@
 import duckduckgo from './duckduckgo'
 import trello from './trello'
 import oracle from './oracle'
+import invision from './invision'
 
 export default {
   duckduckgo,
   trello,
-  oracle
+  oracle,
+  invision
 }
