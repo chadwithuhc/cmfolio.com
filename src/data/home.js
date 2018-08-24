1ff65e95b641f4a4627e79de70348f6487577b47
chadwithuhc <chadwithuhc@gmail.com>
2018-08-23 20:13:58 -0600

    Homepage updates

--- src/data/home.js
+++ src/data/home.js
@@ -1,25 +1,26 @@
 export default {
   name: 'home',
   intro: [
-    `Howdy, I'm <a class="hashlink" title="🧀">cheddar</a>. I am a <a class="hashlink" target="_blank" rel="noopener noreferrer" href="https://denver.gteach.xyz/">teacher</a> by day and <a class="hashlink" target="_blank" rel="noopener noreferrer" href="https://twitter.com/search?l=&q=%23TIL%20from%3Achadwithuhc&src=typd">student</a> all the time 💯`,
-    `🦁 I am happy when being <a class="hashlink">creative</a> and working on lots of <a class="hashlink">ideas</a> at once`
+    `🦁 Howdy, I'm <a class="hashlink" title="🧀">cheddar</a>; a <a class="hashlink" target="_blank" rel="noopener noreferrer" href="https://denver.gteach.xyz/">mentor</a> by day and <a class="hashlink" target="_blank" rel="noopener noreferrer" href="https://twitter.com/search?l=&q=%23TIL%20from%3Achadwithuhc&src=typd">student</a> all the time`,
+    `💯 I am happy when being <a class="hashlink">creative</a> and working on lots of <a class="hashlink">ideas</a> at once `,
+    `🌴 California 🛫 ... 🛬 Denver, CO 🌲`
   ],
   currently_i_am_working_on: [
-    `🤖 Creating a data-driven classroom at Galvanize in Denver, CO`,
-    `✏️ Building fancy educational tools: <em>Everybody Writes 2.0</em>, <a href="https://gshrike.github.io/lp/build/lessons/git-and-github/-L84vgtCB4XdKmUyu4UY/preview"><em>(LP) Lesson Planner</em></a>, <a href="https://terms.galvanize.network/" target="blank" rel="noreferrer noopener"><em>Dev Terms</em></a>`,
-    `🎈 Coding for fun: <a href="https://chadwithuhc.github.io/prototypes/" target="blank" rel="noreferrer noopener"><em>Prototypes</em></a>, <code>this</code> site`,
-    `🏔 Adventuring around <a rel="noopener noreferrer" href="https://www.instagram.com/jfdenver/" class="hashlink">Colorado</a>`,
-    '🕯 Minimizing'
+    `🚀 Bringing healthcare into the 21st century`,
+    `🏫 Building fancy open source educational tools ... <a href="https://gshrike.github.io/lp/build/lessons/git-and-github/-L84vgtCB4XdKmUyu4UY/preview"><em>(LP) Lesson Planner</em></a>, <a href="https://terms.galvanize.network/" target="blank" rel="noreferrer noopener"><em>Dev Terms</em></a>`,
+    `🎈 Coding for fun ... <a href="https://chadwithuhc.github.io/prototypes/" target="blank" rel="noreferrer noopener"><em>Prototypes</em></a>, <code>this</code> site`,
+    // `🏔 Adventuring around <a rel="noopener noreferrer" href="https://www.instagram.com/jfdenver/" class="hashlink">Colorado</a>`,
+    // '🕯 Minimizing'
   ],
   currently_i_am_learning: [
     `🐉 Interactive UIs with VueJS`,
     `🦅 FeathersJS for rapid prototyping`,
-    `🎹 Piano`
+    `🎹 Keyboards & More`
   ],
   recently_i_updated: [
+    `🤖 Codepen playground <a href="https://codepen.io/chadwithuhc" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
     `🎚 Prototype: T-shirt sizes in VueJS <a href="https://chadwithuhc.github.io/prototypes/t-shirt-sizes/" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
     `🎚 Prototype: Heavy Scroller in VueJS <a href="https://chadwithuhc.github.io/prototypes/heavy-scroller/" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
-    `🎭 tattle-tale: a Github App with Probot <a href="https://github.com/chadwithuhc/tattle-tale" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
     `🥋 Leveled up to 5 kyu (95th percentile) on CodeWars <a href="https://www.codewars.com/users/chadwithuhc" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
     `🍻 Pair Programming on Everybody Writes &raquo; <a href="https://www.youtube.com/watch?v=O5A1GAWBV8s" target="blank" rel="noreferrer noopener">Part 1</a> && <a href="https://www.youtube.com/watch?v=cfvSnFYU7sY" target="blank" rel="noreferrer noopener">Part 2</a>`
   ],
