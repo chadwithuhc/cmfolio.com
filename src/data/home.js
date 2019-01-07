27a323c38f5358e2d055f7e8c724e9e51249241e
chadwithuhc <chadwithuhc@gmail.com>
2019-01-06 20:04:03 -0700

    Working on

--- src/data/home.js
+++ src/data/home.js
@@ -2,28 +2,26 @@ export default {
   name: 'home',
   intro: [
     `🦁 Howdy, I'm <a class="hashlink" title="🧀">cheddar</a>; a <a class="hashlink">developer</a> and <a class="hashlink">mentor</a> with a dash of <a class="hashlink">creative</a>`,
-    `💯 I work on lots of <a class="hashlink" href="https://trello.com/b/I55MZ3d3" target="_blank" rel="noopener noreferrer">ideas</a> at once, mostly involving <a href="https://github.com/chadwithuhc" target="_blank" rel="noopener noreferrer" class="hashlink">code</a>, <a class="hashlink">design</a>, or <a class="hashlink" title="user experience">ux</a>`,
+    `🦚 I work on lots of <a class="hashlink" href="https://trello.com/b/I55MZ3d3" target="_blank" rel="noopener noreferrer">ideas</a> at once, mostly involving <a href="https://github.com/chadwithuhc" target="_blank" rel="noopener noreferrer" class="hashlink">code</a>, <a class="hashlink">design</a>, or <a class="hashlink" title="user experience">ux</a>`,
     `🌴 California 🛫 ... 🛬 Denver, CO 🌲`
   ],
   currently_i_am_working_on: [
-    `🏋️ Content Catalog, Accessibility, Testing & Passing Jenkins Builds`,
-    `🎈 Coding for fun ... <a href="https://chadwithuhc.github.io/prototypes/flickr-api/build/#/postcards" target="blank" rel="noreferrer noopener"><em>Postcards</em></a>, <a href="https://codepen.io/chadwithuhc" target="blank" rel="noreferrer noopener"><em>Codepens</em></a>, <em>Mastermind</em>`,
+    `🏋️ Accessibility, Testing & Passing Jenkins Builds`,
+    `🎈 Coding for fun ... <em title="WIP">Tracker</em>, <em title="WIP">Mastermind</em>, <a href="https://codepen.io/chadwithuhc" target="blank" rel="noreferrer noopener"><em>Codepens</em></a>, <a href="https://chadwithuhc.github.io/prototypes/flickr-api/build/#/postcards" target="blank" rel="noreferrer noopener"><em>Postcards</em></a>`,
+    `🕰 Taking my time and keeping it simple`,
+    // `🕺🏻 Creating an alternative portfolio website`
   ],
   currently_i_am_learning: [
-    `🐩 Design with code`,
-    `🎬 Editing & Directing`,
-    `🎹 Keyboards & Percussion`,
+    `🐩 Graphic Design with <a href="https://www.lynda.com/John-McWade/688518-1.html" target="_blank" rel="noopener noreferrer">John McWade</a>`,
+    `🛎 UX with <a href="https://www.lynda.com/Chris-Nodder/1401548-1.html" target="_blank" rel="noopener noreferrer">Chris Nodder</a>`,
+    `🎬 Editing & Directing <small><em>(on the side)</em></small>`,
+    `🎹 Music <small><em>(when I'm away from my computer)</em></small>`,
   ],
   recently_i_updated: [
-    `⏳ UI/UX Concepts: Multiline wrapping <a href="https://codepen.io/chadwithuhc/pen/bQRrLW" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
-    `⏳ UI/UX Concepts: Timeline <a href="https://codepen.io/chadwithuhc/pen/pQRMgO" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
+    `📓 Code Sketchpad: cheddar <a href="https://codepen.io/chadwithuhc/pen/PXKzxb" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
+    `🍕 Meetup Talk: Intro to Regex in JavaScript <a href="https://www.youtube.com/watch?v=rrKF7na-rFc" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
     `🖼 UI/UX Concepts: Photographic Layouts <a href="https://chadwithuhc.github.io/prototypes/flickr-api/" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
-    `🎚 UI/UX Concepts: T-shirt sizes & Heavy Scroller <a href="https://chadwithuhc.github.io/prototypes/" target="_blank" rel="noopener noreferrer">&raquo;&raquo;</a>`,
     `🍻 Pair Programming on Everybody Writes 2.0 &raquo; <a href="https://www.youtube.com/watch?v=O5A1GAWBV8s" target="blank" rel="noreferrer noopener">Part 1</a> && <a href="https://www.youtube.com/watch?v=cfvSnFYU7sY" target="blank" rel="noreferrer noopener">Part 2</a>`,
-  ],
-  lists: [
-    'branding', 'band names', 'rapper names',
-    'inspirational quotes', 'public faces', 'tv'
   ],
   projects: [] // empty for nav bar
 }
